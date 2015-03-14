25dffe66a4f8ce710641d251edbd61a0da5991e1
Masatoshi Teruya <mah0x211@gmail.com>
2015-03-15 00:10:32 +0900

    add: test/hex_try.lua has been added.

---
+++ test/hex_try.lua
@@ -0,0 +1,10 @@
+local hex = require('hex');
+local buffer = require('buffer');
+local str = 'hello world!';
+local b = ifNil( buffer( 100 ) );
+local enc, dec;
+
+ifNotNil( b:add( str ) );
+enc = ifNil( b:hex() );
+dec = ifNil( hex.decode( enc ) );
+ifNotEqual( str, dec );
